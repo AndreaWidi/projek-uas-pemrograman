183f7a0a01a282e1fe95b8fb0fb9bfe808d29166
AndreaWidi <andreawidi886@gmail.com>
2024-11-22 10:08:19 +0700

    barang.h

---
+++ barang.h
@@ -0,0 +1,12 @@
+#ifndef BARANG_H
+#define BARANG_H
+
+void menuBarang();
+void tambahBarang();
+void hapusBarang();
+void tambahStokBarang();
+void kurangiStokBarang();
+void lihatBarang();
+float getHargaBarang(char *nama);
+
+#endif // BARANG_H
